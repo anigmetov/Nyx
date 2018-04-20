6bd2fda1332cb5bca841f30fd1104e6253e98a4c
Ann Almgren <asalmgren@lbl.gov>
2018-04-20 15:50:17 -0700

    Don't use fill_ec_grow in Gravity.

--- Source/Gravity/Gravity.cpp
+++ Source/Gravity/Gravity.cpp
@@ -958,26 +958,15 @@ Gravity::get_old_grav_vector (int       level,
 
     // Fill grow cells in grad_phi, will need to compute grad_phi_cc in 1 grow cell
     const Geometry& geom = parent->Geom(level);
-    if (level == 0)
-    {
-        for (int i = 0; i < BL_SPACEDIM ; i++)
-        {
-            grad_phi_prev[level][i]->setBndry(0);
-            grad_phi_prev[level][i]->FillBoundary(geom.periodicity());
-        }
-    }
-    else
-    {
-        Vector<std::unique_ptr<MultiFab> > crse_grad_phi(BL_SPACEDIM);
-        get_crse_grad_phi(level, crse_grad_phi, time);
-        fill_ec_grow(level, amrex::GetVecOfPtrs(grad_phi_prev[level]),
-	                    amrex::GetVecOfPtrs(crse_grad_phi));
-    }
+
+    // Fill boundary values at the current level
+    for (int i = 0; i < BL_SPACEDIM ; i++)
+       grad_phi_prev[level][i]->FillBoundary(geom.periodicity());
 
     // Average edge-centered gradients to cell centers.
     amrex::average_face_to_cellcenter(grav_vector, 
-				       amrex::GetVecOfConstPtrs(grad_phi_prev[level]),
-				       geom);
+				      amrex::GetVecOfConstPtrs(grad_phi_prev[level]),
+				      geom);
 
 #ifdef CGRAV
     if (gravity_type == "CompositeGrav")
@@ -999,6 +988,8 @@ Gravity::get_old_grav_vector (int       level,
 
     // This is a hack-y way to fill the ghost cell values of grav_vector
     //   before returning it
+    // Note that this fills ghost cells over the coarse grid from interpolation, 
+    //  not from the ghost cell values previously filled after the fill_ec_grow stuff.
     AmrLevel* amrlev = &parent->getLevel(level);
     int ng = grav_vector.nGrow();
     AmrLevel::FillPatch(*amrlev,grav_vector,ng,time,Gravity_Type,0,BL_SPACEDIM);
@@ -1022,21 +1013,9 @@ Gravity::get_new_grav_vector (int       level,
         // Fill grow cells in `grad_phi`, will need to compute `grad_phi_cc` in
         // 1 grow cell
         const Geometry& geom = parent->Geom(level);
-        if (level == 0)
-        {
-            for (int i = 0; i < BL_SPACEDIM ; i++)
-            {
-                grad_phi_curr[level][i]->setBndry(0);
-                grad_phi_curr[level][i]->FillBoundary(geom.periodicity());
-            }
-        }
-        else
-        {
-            Vector<std::unique_ptr<MultiFab> > crse_grad_phi(BL_SPACEDIM);
-            get_crse_grad_phi(level, crse_grad_phi, time);
-            fill_ec_grow(level, amrex::GetVecOfPtrs(grad_phi_curr[level]),
-                                amrex::GetVecOfPtrs(crse_grad_phi));
-        }
+
+        for (int i = 0; i < BL_SPACEDIM ; i++)
+            grad_phi_curr[level][i]->FillBoundary(geom.periodicity());
 
         // Average edge-centered gradients to cell centers, excluding grow cells
         amrex::average_face_to_cellcenter(grav_vector,
@@ -1068,6 +1047,8 @@ Gravity::get_new_grav_vector (int       level,
 
     // This is a hack-y way to fill the ghost cell values of grav_vector
     //   before returning it
+    // Note that this fills ghost cells over the coarse grid from interpolation, 
+    //  not from the ghost cell values previously filled after the fill_ec_grow stuff.
     AmrLevel* amrlev = &parent->getLevel(level) ;
     int ng = grav_vector.nGrow();
     AmrLevel::FillPatch(*amrlev,grav_vector,ng,time,Gravity_Type,0,BL_SPACEDIM);
